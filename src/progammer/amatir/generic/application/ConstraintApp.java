8eec24db25899e5460a5d7c8fdd1200b02b60034
nanandt <rizkyfatih007@gmail.com>
2021-05-02 13:16:04 +0700

    bounded type parameter

---
+++ src/progammer/amatir/generic/application/ConstraintApp.java
@@ -0,0 +1,26 @@
+package progammer.amatir.generic.application;
+
+public class ConstraintApp {
+    public static void main(String[] args) {
+        NumberData<Integer> numberData = new NumberData<>(1);
+        NumberData<Long> longNumberData = new NumberData<>(1L);
+
+        // NumberData<String> stringNumberData = new NumberData<>("haha"); // ERROR krn sdh diset tipe datanya Number
+    }
+
+    public static class NumberData<T extends Number>{
+        private T data;
+
+        public NumberData(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+}
---
+++ src/progammer/amatir/generic/application/MultipleConstraintApp.java
@@ -0,0 +1,47 @@
+package progammer.amatir.generic.application;
+
+public class MultipleConstraintApp {
+    public static void main(String[] args) {
+        // Data<Manager> managerData = new Data<>(new Manager()); // ERROR krn Manager tdk implement sayHello
+        Data<VicePresident> vicePresidentData = new Data<>(new VicePresident());
+
+    }
+
+    public static interface CanSayHello {
+        void sayHello(String name);
+    }
+
+    public static abstract class Employee {
+
+    }
+
+    public static class Manager extends Employee {
+
+    }
+
+    public static class VicePresident extends Employee implements CanSayHello {
+
+
+        @Override
+        public void sayHello(String name) {
+            System.out.println("Hello " + name);
+        }
+    }
+
+    public static class Data<T extends Employee & CanSayHello>{
+        private T data;
+
+        public Data(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+            this.data.sayHello("Bayu");
+        }
+    }
+}
